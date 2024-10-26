b138f063436cc0fbd01340c61dc0e6df5afd14ed
Soumyajit Pan <soumyajit.pan@ad.infosys.com>
2024-10-26 18:09:16 +0530

    add resources method

--- Testcases/test_login_page.py
+++ Testcases/test_login_page.py
@@ -1,11 +1,14 @@
-import time
 import pytest
+from Resources.resource_codes import *
 
-@pytest.mark.test
+
+@pytest.mark.test1
 def test_scenario_1():
-    print("Test Scenario")
+    print("Test Scenario1")
+    scenario_method()
     assert True
-    
+
+
 @pytest.mark.test2
 def test_scenario_2():
     print("Test Scenario 2")
