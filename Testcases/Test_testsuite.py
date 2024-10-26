2aa209ab79bd0e3f8a91bddb170e3eefb0a55a0d
Soumyajit Pan <soumyajit.pan@ad.infosys.com>
2024-10-26 17:37:22 +0530

    upload

--- Testcases/Test_testsuite.py
+++ Testcases/Test_testsuite.py
@@ -7,3 +7,9 @@ def test_scenario_1():
     print("Test Scenario")
     scenario_method()
     assert True
+    
+    
+@pytest.mark.test2
+def test_scenario_2():
+    print("Test Scenario 2")
+    assert True        
