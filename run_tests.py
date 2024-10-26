75259d519a92dd5cb772b3fcdb81ea4c1c1b87f2
Soumyajit Pan <soumyajit.pan@ad.infosys.com>
2024-10-26 17:34:51 +0530

    upload

--- run_tests.py
+++ run_tests.py
@@ -4,15 +4,16 @@ import pytest
 import sys
 
 def main():
+    print("run_tests is running...")
     system_arguments = sys.argv
     try:
         if len(system_arguments) > 1:
             tags = system_arguments[1:]
             for tag in tags:
                 print(f"Running tests with tag: {tag}")
-                pytest.main(['-m', tag, '-v', f'--html=Reports/Report_{tag}.html'.format(tag)])
+                pytest.main(['-m', tag, '-v', f'--html=Reports/Reports_{tag}.html'.format(tag)])
         else:
-            pytest.main(['-v', f'--html=Reports/Report.html'])
+            pytest.main(['-v', f'--html=Reports/Reports.html'])
     except Exception as e:
         print(f"Something went wrong: {e}")
         
